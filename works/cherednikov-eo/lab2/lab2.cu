dbc8a2a6eef098596890dfcf8f1ca241a4c6678c
echerednikov <echerednikov@diasoft.ru>
2025-11-25 20:32:32 +0300

    lab2 2 almost done

--- works/cherednikov-eo/lab2/lab2.cu
+++ works/cherednikov-eo/lab2/lab2.cu
@@ -9,18 +9,21 @@
 #include <cuda_runtime.h>
 #include "error_check.cuh"
 
+// &A - указатель на m*n матрицу A
+// &B - указатель на n*k матрицу B
+// &C - выходной указатель на результирующую m*k матрицу C
 
 //Базовая версия на CPU
-void cpu_matmul(const float* A, const float* B, float* C, int M, int N, int K) {
+void cpu_matmul(int* A, int* B, int* C, int m, int n, int m) {
     // Создаём C
-    for (int i = 0; i < M*K; ++i) C[i] = 0.0f;
+    for (int i = 0; i < m*k; ++i) C[i] = 0;
 
-    for (int i = 0; i < M; ++i) {
-        for (int p = 0; p < N; ++p) {
-            float aip = A[i*N + p];
+    for (int i = 0; i < m; ++i) {
+        for (int p = 0; p < n; ++p) {
+            int aip = A[i*n + p];
             // перемножаем строку i матрицы A со строкой p матрицы B (B строка p относитя к колонкам)
-            const float* Bp = B + p*K;
-            float* Ci = C + i*K;
+            int* Bp = B + p*k;
+            int* Ci = C + i*k;
             for (int j = 0; j < K; ++j) {
                 Ci[j] += aip * Bp[j];
             }
@@ -29,28 +32,49 @@ void cpu_matmul(const float* A, const float* B, float* C, int M, int N, int K) {
 }
 
 
-
 //Базовая версия перемножения на GPU без оптимизации
-// &a - GPU указатель на m*n матрицу A
-// $b - GPU указатель на n*k матрицу B
-// &c - GPU выходной указатель на результирующую m*k матрицу C
-__global__ void gpu_matrix_mult(int *a,int *b, int *c, int m, int n, int k)
+__global__ void gpu_naive_mm(int *A,int *B, int *C, int m, int n, int k)
 {
     int row = blockIdx.y * blockDim.y + threadIdx.y;
     int col = blockIdx.x * blockDim.x + threadIdx.x;
+	if (row >= M || col >= K) return;
+
     int sum = 0;
-    if( col < k && row < m)
-    {
-        for(int i = 0; i < n; i++)
-        {
-            sum += a[row * n + i] * b[i * k + col];
-        }
-        c[row * k + col] = sum;
+    for(int i = 0; i < n; i++) {
+    	sum += A[row * n + i] * B[i * k + col];
     }
+    C[row * k + col] = sum;
 }
 
 
+#ifndef TILE
+#define TILE 32
+#endif
 
+// Оптимизированная версия перемножения матриц
+__global__ void gpu_optimised_mm(int *A,int *B, int *C, int m, int n, int k)
+{
+    __shared__ int sA[TILE][TILE+1];
+    __shared__ int sB[TILE][TILE+1];
+
+    int row = blockIdx.y * TILE + threadIdx.y;
+    int col = blockIdx.x * TILE + threadIdx.x;
+
+    int sum = 0;
+
+	//Tile A: rows: [blockY*TILE .. blockY*TILE+TILE-1], cols: [m*TILE .. m*x]
+
+	//Проходимся по всем тайлам
+    for(int i = 0; i < gridDim.x; ++i) {
+
+		idx = row * n + i * TILE + threadIdx.x;
+
+		if (idx )
+
+    	sum += A[row * n + i] * B[i * k + col];
+    }
+    C[row * k + col] = sum;
+}
 
 
 int main() {
