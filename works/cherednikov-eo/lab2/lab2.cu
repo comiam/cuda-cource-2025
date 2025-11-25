aa4184c901a6b673241c8646b278271fa8ca269f
echerednikov <echerednikov@diasoft.ru>
2025-11-25 15:55:03 +0300

    lab2 2 functions

--- works/cherednikov-eo/lab2/lab2.cu
+++ works/cherednikov-eo/lab2/lab2.cu
@@ -1,39 +1,60 @@
 #include <cstdio>
 #include <cstdlib>
+#include <cmath>
+#include <chrono>
+#include <random>
+#include <iostream>
+#include <vector>
+#include <algorithm>
 #include <cuda_runtime.h>
 #include "error_check.cuh"
 
 
-//__global__ void hello_kernel() {
-    // device-side printf разрешён: можно логировать поток/блок.
-//   printf("Block(%d,%d,%d) Thread(%d,%d,%d)\n",
-//           blockIdx.x, blockIdx.y, blockIdx.z,
-//           threadIdx.x, threadIdx.y, threadIdx.z);
-//}
+//Базовая версия на CPU
+void cpu_matmul(const float* A, const float* B, float* C, int M, int N, int K) {
+    // Создаём C
+    for (int i = 0; i < M*K; ++i) C[i] = 0.0f;
 
-
-__global__ void draw_square(char* canvas, int w, int h, int margin) {
-    int x = blockIdx.x * blockDim.x + threadIdx.x;
-    int y = blockIdx.y * blockDim.y + threadIdx.y;
-    if (x >= w || y >= h) return;
-
-    int idx = y * w + x;
-
-    // Условие: принадлежит ли пиксель границе квадрата
-    bool inside = (x >= margin && x < w - margin && y >= margin && y < h - margin);
-    bool border = (x == margin || x == w - margin - 1 || y == margin || y == h - margin - 1);
-
-    if (inside && border)
-        canvas[idx] = '*';
-    else
-        canvas[idx] = ' ';
+    for (int i = 0; i < M; ++i) {
+        for (int p = 0; p < N; ++p) {
+            float aip = A[i*N + p];
+            // перемножаем строку i матрицы A со строкой p матрицы B (B строка p относитя к колонкам)
+            const float* Bp = B + p*K;
+            float* Ci = C + i*K;
+            for (int j = 0; j < K; ++j) {
+                Ci[j] += aip * Bp[j];
+            }
+        }
+    }
 }
 
 
+
+//Базовая версия перемножения на GPU без оптимизации
+// &a - GPU указатель на m*n матрицу A
+// $b - GPU указатель на n*k матрицу B
+// &c - GPU выходной указатель на результирующую m*k матрицу C
+__global__ void gpu_matrix_mult(int *a,int *b, int *c, int m, int n, int k)
+{
+    int row = blockIdx.y * blockDim.y + threadIdx.y;
+    int col = blockIdx.x * blockDim.x + threadIdx.x;
+    int sum = 0;
+    if( col < k && row < m)
+    {
+        for(int i = 0; i < n; i++)
+        {
+            sum += a[row * n + i] * b[i * k + col];
+        }
+        c[row * k + col] = sum;
+    }
+}
+
+
+
+
+
 int main() {
-    int width = 28;
-    int height = 28;
-    int margin = 2;   // толщина отступа от края
+    int m, n, k;
 
     size_t size = width * height;
     char* h_canvas = (char*)malloc(size);
@@ -48,15 +69,5 @@ int main() {
     draw_square<<<grid, block>>>(d_canvas, width, height, margin);
     CUDA_CHECK(cudaDeviceSynchronize());
 
-    cudaMemcpy(h_canvas, d_canvas, size, cudaMemcpyDeviceToHost);
-
-    // Печать квадрата
-    for (int y = 0; y < height; y++) {
-        fwrite(&h_canvas[y * width], 1, width, stdout);
-        putchar('\n');
-    }
-
-    cudaFree(d_canvas);
-    free(h_canvas);
     return 0;
 }
